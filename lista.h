fbd0eca5e37a41f34f317e925586bc00536379b6
Axel Lopez <axlopez@estudiantec.cr>
2023-09-13 13:43:57 -0600

    Actualización

--- lista.h
+++ lista.h
@@ -1,66 +1,175 @@
-#ifndef lista_h
-#define lista_h 0
+#include "Node.h"
+#include "../eventos/event.h"
+#include "stack.h"
+#include "queue.h"
+#include <unordered_map>
 
-#include <iostream>
-#include <string>
-#include <stdlib.h>
-#include "nodo.h"
+#ifndef LIST 
 
-string dato;
-int rActual;
-int rNueva;
+#define LIST 1
 
-struct lista{
+using namespace std;
+
+template <typename T> 
+class List : public Stack<T>, public Queue<T> {
     private:
-        struct nodo* head = nullptr;
-        struct nodo* tail = nullptr;
-        int size = 0;
+        Node<T> *first;
+        Node<T> *last;
+        Node<T> *searchPosition;
+        unordered_map<int, Node<T>*> nodeIndex; 
+        int size;
 
     public:
-        void agregar(void *pDato){
-           /* struct nodo *nNuevo = (struct nodo *)malloc(sizeof(struct nodo));
-            nNuevo->data = pDato;
+        List() {
+            first = NULL;
+            last = NULL;
+            size = 0;
+        }
 
-            if(size == 0){
-                head = nNuevo;
-                tail = nNuevo;
+        Node<T>* getFirst() {
+            return this->first;
+        }
+
+        int getSize() {
+            return size;
+        }
+
+        bool isEmpty() {
+            return !size;
+        }
+
+        T* find(int pPosition) {
+            T* result = NULL;
+            searchPosition = NULL;
+
+            if (nodeIndex.find(pPosition) != nodeIndex.end()) {
+                searchPosition = nodeIndex[pPosition];
+                result = searchPosition->getData();
             }
-            else{
-                nNuevo -> ant = tail;
-                tail -> sig = nNuevo;
-                tail = nNuevo;
+
+            return result;
+        }
+
+        void add(T *pData) {
+            Node<T> *newNode = new Node<T>(pData);
+
+            if (size>0) {
+                this->last->setNext(newNode);
+                newNode->setPrev(this->last);  // esta linea se agrega para quiz #4 #5
+            } else {
+                this->first = newNode;
             }
+            this->last = newNode;
+
+            nodeIndex[size] = newNode; // save for indexing
             size++;
-            */
         }
 
-
-        void *eliminar(){
-            /*
-            void *result = nullptr;
-            struct nododoble *cursor = head;
-     
+        // si el position es mayor a la cantidad, entonces inserto al final
+        void insert(int pPosition, T *pData) {
             
-            while(aux != nullptr){
-                if(aux->titulo == dato){
-                    if(aux == head){
-                        head = head->sig;
-                        head->ant = nullptr;
-                    }else if(aux == tail){
-                        tail = tail->ant;
-                        tail->sig = nullptr;
-                    }else{
-                        aux->ant->sig = aux->sig;
-                        aux->sig->ant = aux->ant;
-                    }
-                    delete aux;
-                    return;
+            if (pPosition<getSize() && first!=NULL) {
+                Node<T> *newNodo = new Node<T>(pData);
+
+                T* result = find(pPosition); 
+                // esta linea se quita para quiz #4 #5, ya no hace falta, aqui iba searchBehind
+                
+                newNodo->setNext(searchPosition);
+                if (searchPosition->getPrev()!=NULL) {  // apariciones de searchBehind ahora serian getPrev de searchPosition, quiz #4 #5
+                    searchPosition->getPrev()->setNext(newNodo); // esas lineas se arreglaan con el prev #4 #5
+                    newNodo->setPrev(searchPosition->getPrev());
+                    searchPosition->setPrev(newNodo);  
+                } else {
+                    this->first->setPrev(newNodo); // quiz #4 #5
+                    this->first = newNodo;
                 }
-                aux = aux->sig;
+                
+                for(int indexToAdjust=getSize()-1; indexToAdjust>=pPosition; indexToAdjust--) {
+                    if (nodeIndex.find(indexToAdjust) != nodeIndex.end()) {
+                        nodeIndex[indexToAdjust+1] = nodeIndex[indexToAdjust]; 
+                    }
+                }
+
+                nodeIndex[pPosition] = newNodo;
+                size++;
+            } else {
+                add(pData);
             }
-            */
         }
 
+        T* remove(int pPosition) {
+            T* result = NULL;
+            if (first!=NULL && pPosition<getSize()) {
+                searchPosition = first;
+
+                if (pPosition!=0) {
+                    T* data = find(pPosition);
+
+                    searchPosition->getPrev()->setNext(searchPosition->getNext()); // quiz #4 #5, se quita searchBehind
+
+                    if (searchPosition==last) {
+                        last = searchPosition->getPrev();  // quiz #4 #5
+                    } else {
+                        searchPosition->getNext()->setPrev(searchPosition->getPrev()); // quiz #4 #5
+                    }
+                    searchPosition->setNext(NULL);
+                    searchPosition->setPrev(NULL);
+
+                    result = searchPosition->getData();
+                } else {
+                    first = first->getNext();
+                    searchPosition->setNext(NULL);
+
+                    if (first!=NULL) { // quiz #4 #5, hay que limpiar el anterior del que esta despues del primero
+                        first->setPrev(NULL); 
+                    } else {
+                        last = NULL;
+                    }
+                    result = searchPosition->getData();
+                }
+
+                int indexToAdjust=pPosition;
+                for(; indexToAdjust<getSize()-1; indexToAdjust++) {
+                    if (nodeIndex.find(indexToAdjust) != nodeIndex.end()) {
+                        nodeIndex[indexToAdjust] = nodeIndex[indexToAdjust+1]; 
+                    }
+                }
+
+                nodeIndex.erase(indexToAdjust+1);
+
+                size--;
+            }
+            return result;
+        };
+
+        void push(T* pValue) {
+            insert(0, pValue);
+        }
+
+        T* pop() {
+            return remove(0);
+        }
+
+        void enqueue(T* pValue) {
+            add(pValue);
+        }
+
+        T* dequeue() {
+            return remove(0);
+        }
+
+        void resetSearch() {
+            searchPosition = first;
+        }
+
+        T* next() {
+            T* result = NULL;
+            if (searchPosition!=NULL) {
+                result = searchPosition->getData();
+                searchPosition = searchPosition->getNext();
+            }
+            return result;
+        }
 
 };
 
--- nodo.h
+++ nodo.h
@@ -1,17 +1,62 @@
-#ifndef nodo_h
-#define nodo_h 2
-
 #include <iostream>
-#include <string>
+
+#ifndef NODE 
+
+#define NODE 1
 
 using namespace std;
 
+template <class T>
+class Node {
+    private:
+        T *data;
+        Node *next;
+        Node *previous;    //modificacion, agrego link hacia atras 
+    
+    public:
+        Node() {
+            data = NULL;
+            next = NULL;
+            previous = NULL;   //modif, creo previous y la hago null
+        }
 
-struct nododoble {
-    void* data = nullptr;
-    struct nodo* sig = nullptr;
-    struct nodo* ant = nullptr;
+        Node(T *pData) {
+            this->data = pData;
+            next = NULL;
+            previous = NULL;  //modif, la pongo en null
+        }
 
+        T* getData() {
+            return data;
+        }
+
+        void setData(T *pData) {
+            this->data = pData;
+        }
+
+        Node* getNext() {
+            return next;
+        }
+
+        Node* getPrevious() {    //modif, creo este metodo que retorna
+            return previous;     //el previo
+        }
+
+        void setNext(Node *pValue) {
+            this->next = pValue;
+        }
+
+        void setPrevious(Node *pValue) {   //modif, creo este metodo que
+            this->previous = pValue;       //pone valor al previo    
+        }
+
+        Node* getPrev() {    //modif, creo este metodo que retorna
+            return previous;     //el previo
+        }
+
+        void setPrev(Node *pValue) {   //modif, creo este metodo que
+            this->previous = pValue;       //pone valor al previo    
+        }
 };
 
 #endif

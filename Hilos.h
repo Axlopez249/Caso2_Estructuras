294b99fce8e5b6aed894322deb45c4a67ce6fc01
Axel Lopez <axlopez@estudiantec.cr>
2023-09-24 19:26:40 -0600

    hilos

--- Hilos.h
+++ Hilos.h
@@ -2,28 +2,32 @@
 #include <string>
 #include <vector>
 #include "lista.h"
+#include "queue.h"
+#include "stack.h"
 #include "carro.h"
 #include "reseta.h"
 #include <fstream>
 #include <nlohmann/json.hpp>
+//son para generar los numeros aleatorios
+#include <cstdlib> // Necesario para srand() y rand()
+#include <ctime>   // Necesario para time()
+//estos son para el hilo
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
-void generar_carros(){
+void generar_carros(vector<Reseta>& presetas,vector<Carro>& carros){
 
     ifstream archivo_json("JSON.json");
 
     if (!archivo_json.is_open()) {
         cerr << "No se pudo abrir el archivo JSON." << endl;
-        return 1;
+        return;
     }
 
     json configuracion;
     archivo_json >> configuracion;
-
-    vector<Carro> carros;
-    vector<Reseta> resetas;
-
     
     json carros_json = configuracion["Configuracion"]["Carros"];
     json resetas_json = configuracion["Configuracion"]["Resetas"];
@@ -36,7 +40,7 @@ void generar_carros(){
 
         // Crear un objeto Reseta y agregarlo a la lista
         Reseta reseta = new Reseta(nombre, precio, ingredientes);
-        resetas.push_back(reseta);
+        presetas.push_back(reseta);
 
     } 
 
@@ -45,6 +49,31 @@ void generar_carros(){
         Carro carro = new Carro(carro_json["Nombre"], carro_json["Hora"]);
         carros.push_back(carro);
     }
+    return;
+}
+
+void ingresar_cola(vector<Carro> &pcarro, vector<Reseta> &preseta, Queue<string[]> *pcola_carros){
+
+    int contador=0;
+    string[2] elemento_cola;
+
+    while (contador<=5){
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+        // Generar un número aleatorio entre 0 y 3
+        int numeroAleatorio = std::rand() % 4;
+
+        elemento_cola[0] = pcarro[contador];
+        elemento_cola[1] = preseta[numeroAleatorio];
+        pcola_carros->enqueue(&elemento_cola);
+        contador++;
+
+        // Pausa el hilo durante un segundo
+        this_thread::sleep_for(chrono::seconds(5));
+    }
+    
+    
+}
 
     //Falta relacionar las resetas con los carros
     //Falta crear un tiempo
@@ -52,4 +81,7 @@ void generar_carros(){
     //Que llegue a la ventanilla con su cierto tiempo
     //Hay que agregar en el json el tiempo que se tarda en la realidad y en la simulacion
     //por ejemplo en realidad 10 segundos son 5 minutos y así para que tenga fluidez
-}
+
+    //Secuencia
+    //saco datos carros y resetas
+    //El carro tiene que llevar el nombre de la orden
--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 #include <string>
 #include <vector>
 #include "lista.h"
+#include "Hilos.h"
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 int main(){
+    vector<Reseta> resetas;
+    vector<Carro> carros;
+
+    Queue<string[]> *cola_carros = new List<string[]>();
+
+    generar_carros(resetas, carros);
+
+    thread hilo(ingresar_cola(carros, resetas, cola_carros));
+
+    // Esperar a que el hilo continúe ejecutándose
+    hilo.join();
+    return 0;
 
 }
